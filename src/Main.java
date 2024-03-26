40c339a5729e1ca7d091900e08d8013acdd0a6c0
Lenilson Soares <119903522+LenilsonSoares@users.noreply.github.com>
2024-03-26 13:32:33 -0300

    meu primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bem-vindo ao meu primeiro projeto em Java!");
+        Scanner
 
-        System.out.println("Este é meu primeiro projeto em Java!");
-    }
 }
