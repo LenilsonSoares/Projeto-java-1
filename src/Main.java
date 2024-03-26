b9f74f10460d37d208402408f4419e77dbd7f013
Lenilson Soares <119903522+LenilsonSoares@users.noreply.github.com>
2024-03-26 16:59:24 -0300

    meu primeiro commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,75 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        saudacaoUsuario();
+        menu();
+    }
+
+    public static void saudacaoUsuario() {
+
         System.out.println("Bem-vindo ao meu primeiro projeto em Java!");
-        Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "! É um prazer te conhecer.");
+    }
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Tabuada");
+            System.out.println("2. Soma");
+            System.out.println("3. Sair");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    tabuada();
+                    break;
+                case 2:
+                    soma();
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 3);
+    }
+
+    public static void tabuada() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite um número:");
+        int numero = scanner.nextInt();
+
+        if (numero < 1 || numero > 10) {
+            System.out.println("Por favor, digite um numero entre 1 e 10.");
+        } else {
+            System.out.println("A tabuada do número " + numero + " é:");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+        }
+    }
+
+    public static void soma() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o primeiro número:");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Por favor, digite um segundo número:");
+        int numero2 = scanner.nextInt();
+
+        int resultado = numero1 + numero2;
+        System.out.println("A soma de " + numero1  + " e " + numero2 + " é " + resultado + "." );
+    }
 
 }
